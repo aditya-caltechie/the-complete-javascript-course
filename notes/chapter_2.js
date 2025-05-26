4823278ade99392dc7991ed0f65a987ad423e2fc
Aditya Verma <aditya.verma@bittorrent.com>
2025-05-25 18:20:08 -0700

    My first JS commit

--- notes/chapter_2.js
+++ notes/chapter_2.js
@@ -1,39 +1,12 @@
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 'hello world'
-console.log()
+console.log('Hello Aditya')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let example_variable = 'this is a string' // this is a string
+let example_number = 123 // this is a number
+let example_float = 123.456 // this is a float
+let example_boolean = true // this is a boolean
+let example_null = null // this is a null value
 
 481212398123 // this is number
 
@@ -51,20 +24,18 @@ let is_true = true // could be false
 
 
 function myFirstFunction(value, secondValue) {
-    // output the value of is_true
-    console.log(value != secondValue)
+    console.log('The value is: ', value)
+    console.log('The second value is: ', secondValue)
+    //console.log(value != secondValue)
 }
 
 
 let multiplyTwoNumbers = myFirstFunction
-
-// multiplyTwoNumbers(3, 5)
-// multiplyTwoNumbers('6', 6)
-// multiplyTwoNumbers(7, 5)
+multiplyTwoNumbers(3, 5)
+multiplyTwoNumbers('6', 6)
 
 
 // && AND || OR !
-
 let condition1 = true
 let condition2 = false
 
@@ -79,24 +50,19 @@ if (condition2 && condition1) {
 }
 
 let example_array = [1, 5, 7, 9, 4, 5, 6, 7, 3, 4, 5, 2342]
-
-
 let i = 0
 let length = example_array.length
-// while (i < length) {
-//     console.log('value was true: ', i, example_array[i])
-//     i++
-// }
+while (i < length) {
+    //console.log('value was true: ', i, example_array[i])
+    i++
+}
 
-// for (let j = 0; j < length; j++) {
-//     if (j === 6) {
-//         break
-//     }
-//     console.log('value was true: ', j, example_array[j])
-
-
-// }
-
+for (let j = 0; j < length; j++) {
+    if (j === 6) {
+        break
+    }
+    //console.log('value was true: ', j, example_array[j])
+}
 
 
 function addStrings(string1 = 'default1', string2 = 'default2') {
